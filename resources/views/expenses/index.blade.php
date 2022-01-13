62e42d8e5556bc0e3bdac2784c7002faae2cd1b3
KazushiNitta <kazushi.sg.gr@gmail.com>
2022-01-13 19:12:16 +0900

    一覧ページのスタイル追加

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -3,30 +3,19 @@
         収支一覧
     </x-slot>
 
-    <section>
-        <a href="{{ route('incomes.create') }}">収入を登録する</a>
-        <a href="{{ route('expenses.create') }}">支出を登録する</a>
+    <section class="register">
+        <a href="{{ route('incomes.create') }}" class="income">収入を登録する</a>
+        <a href="{{ route('expenses.create') }}" class="expense">支出を登録する</a>
     </section>
 
-    {{-- <section>
-        <ul class="list-menu">
-            <li>
-                <a href="{{ route('incomes.index') }}">収入の詳細を確認する</a>
-            </li>
-            <li>
-                <a href="{{ route('expenses.index') }}">支出の詳細を確認する</a>
-            </li>
-        </ul>
-    </section> --}}
-
-    <section>
+    <section class="search">
         <form action="{{ route('expenses.index') }}" method="GET">
             <input type="month" name="month" value="{{ $month }}">
             <button>絞り込み</button>
         </form>
     </section>
 
-    <section>
+    <section class="sum">
         <table>
             <tr>
                 <th>収入</th>
@@ -45,26 +34,27 @@
         </table>
     </section>
 
-    <section>
+    <section class="menu">
         <ul class="list-menu">
             <li>
-                <a href="{{ route('incomes.index') }}">収入</a>
+                <a href="{{ route('incomes.index') }}" class="expense-income">収入</a>
             </li>
             <li>
-                <a href="{{ route('expenses.index') }}">支出</a>
+                <a href="{{ route('expenses.index') }}" class="expense-expense">支出</a>
             </li>
         </ul>
     </section>
 
-    <section>
-        {{-- <h2>支出一覧</h2> --}}
-        <table border="1">
+    <section class="content">
+        <table border="1" class="expense_table">
             @if (count($expenses) > 0)
-                <tr>
-                    <th>日付</th>
-                    <th>科目</th>
+                <tr class="expense">
+                    <th width="150px">日付</th>
+                    <th width="150px">科目</th>
                     <th>摘要</th>
-                    <th>金額</th>
+                    <th width="150px">金額</th>
+                    <th width="100px"></th>
+                    <th width="100px"></th>
                 </tr>
                 @foreach ($expenses as $expense)
                     <tr>
@@ -72,7 +62,11 @@
                         <td>{{ $expense->account }}</td>
                         <td>{!! nl2br(e($expense->text)) !!}</td>
                         <td>¥{{ $expense->price }}</td>
-                        <td><a href="{{ route('expenses.edit', $expense) }}">編集</a></td>
+                        <td>
+                            <button type="button">
+                                <a href="{{ route('expenses.edit', $expense) }}">編集</a>
+                            </button>
+                        </td>
                         <td>
                             <form action="{{ route('expenses.destroy', $expense) }}" method="POST">
                                 @method('DELETE')
--- resources/views/incomes/index.blade.php
+++ resources/views/incomes/index.blade.php
@@ -3,30 +3,19 @@
         収支一覧
     </x-slot>
 
-    <section>
-        <a href="{{ route('incomes.create') }}">収入を登録する</a>
-        <a href="{{ route('expenses.create') }}">支出を登録する</a>
+    <section class="register">
+        <a href="{{ route('incomes.create') }}" class="income">収入を登録する</a>
+        <a href="{{ route('expenses.create') }}" class="expense">支出を登録する</a>
     </section>
 
-    {{-- <section>
-        <ul class="list-menu">
-            <li>
-                <a href="{{ route('incomes.index') }}">収入の詳細を確認する</a>
-            </li>
-            <li>
-                <a href="{{ route('expenses.index') }}">支出の詳細を確認する</a>
-            </li>
-        </ul>
-    </section> --}}
-
-    <section>
+    <section class="search">
         <form action="{{ route('incomes.index') }}" method="GET">
             <input type="month" name="month" value="{{ $month }}">
             <button>絞り込み</button>
         </form>
     </section>
 
-    <section>
+    <section class="sum">
         <table>
             <tr>
                 <th>収入</th>
@@ -45,26 +34,27 @@
         </table>
     </section>
 
-    <section>
+    <section class="menu">
         <ul class="list-menu">
             <li>
-                <a href="{{ route('incomes.index') }}">収入</a>
+                <a href="{{ route('incomes.index') }}" class="income-income">収入</a>
             </li>
             <li>
-                <a href="{{ route('expenses.index') }}">支出</a>
+                <a href="{{ route('expenses.index') }}" class="income-expense">支出</a>
             </li>
         </ul>
     </section>
 
-    <section>
-        {{-- <h2>収入一覧</h2> --}}
-        <table border="1">
+    <section class="content">
+        <table border="1" class="income_table">
             @if (count($incomes) > 0)
-                <tr>
-                    <th>日付</th>
-                    <th>科目</th>
+                <tr class="income">
+                    <th width="150px">日付</th>
+                    <th width="150px">科目</th>
                     <th>摘要</th>
-                    <th>金額</th>
+                    <th width="150px">金額</th>
+                    <th width="100px"></th>
+                    <th width="100px"></th>
                 </tr>
                 @foreach ($incomes as $income)
                     <tr>
@@ -72,7 +62,11 @@
                         <td>{{ $income->account }}</td>
                         <td>{!! nl2br(e($income->text)) !!}</td>
                         <td>¥{{ $income->price }}</td>
-                        <td><a href="{{ route('incomes.edit', $income) }}">編集</a></td>
+                        <td>
+                            <button type="button">
+                                <a href="{{ route('incomes.edit', $income) }}">編集</a>
+                            </button>
+                        </td>
                         <td>
                             <form action="{{ route('incomes.destroy', $income) }}" method="POST">
                                 @method('DELETE')
