ab6452cae9c35a5d96af36431f766b99e7a01975
KazushiNitta <kazushi.sg.gr@gmail.com>
2022-01-14 16:05:09 +0900

    登録、編集ページのスタイルを追加

--- resources/views/expenses/edit.blade.php
+++ resources/views/expenses/edit.blade.php
@@ -3,8 +3,8 @@
         支出編集
     </x-slot>
 
-    <h2>支出編集</h2>
-    <section>
+    <h2 class="sub-title expense">支出編集</h2>
+    <section class="form">
         <form action="{{ route('expenses.update', $expense) }}" method="POST">
             @method('PATCH')
             @csrf
@@ -15,27 +15,27 @@
                 @endforeach
             </ul>
 
-            <div>
-                <label for="date">日付</label>
-                <input type="date" name="date" id="date" value="{{ old('date', $expense->date) }}">
+            <div class="part">
+                <label for="date" class="label">日付</label>
+                <input type="date" name="date" id="date" value="{{ old('date', $expense->date) }}" class="input">
             </div>
 
-            <div>
-                <label for="account">科目</label>
-                <input type="text" name="account" id="account" value="{{ old('account', $expense->account) }}">
+            <div class="part">
+                <label for="account" class="label">科目</label>
+                <input type="text" name="account" id="account" value="{{ old('account', $expense->account) }}" class="input">
             </div>
 
-            <div>
-                <label for="text">摘要</label>
-                <textarea name="text" id="text">{{ old('text', $expense->text) }}</textarea>
+            <div class="part">
+                <label for="text" class="label">摘要</label>
+                <textarea name="text" id="text" class="text">{{ old('text', $expense->text) }}</textarea>
             </div>
 
-            <div>
-                <label for="price">金額</label>
-                <input type="number" name="price" id="price" value="{{ old('price', $expense->price) }}">
+            <div class="part">
+                <label for="price" class="label">金額</label>
+                <input type="number" name="price" id="price" value="{{ old('price', $expense->price) }}" class="input">
             </div>
-            <button>編集</button>
+            <button class="expense">編集</button>
         </form>
+        <a href="{{ route('expenses.index') }}">戻る</a>
     </section>
-    <a href="{{ route('expenses.index') }}">戻る</a>
 </x-layout>
